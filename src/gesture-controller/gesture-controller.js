22cf2a252ba87ac9363d4d727d5e53b287b947d1
Artem Maksimov <artem@getcodified.com>
2017-07-16 12:26:55 +0300

    #2 IE11 fix

--- src/gesture-controller/gesture-controller.js
+++ src/gesture-controller/gesture-controller.js
@@ -1,6 +1,7 @@
 ym.modules.define('shri2017.imageViewer.GestureController', [
-    'shri2017.imageViewer.EventManager'
-], function (provide, EventManager) {
+    'shri2017.imageViewer.EventManager',
+    'util.extend'
+], function (provide, EventManager, extend) {
 
     var DBL_TAB_STEP = 0.2;
     var ZOOM_DELTA_COEF = 0.001;
@@ -16,7 +17,7 @@ ym.modules.define('shri2017.imageViewer.GestureController', [
         this._onTouchZoomEnabled = false;
     };
 
-    Object.assign(Controller.prototype, {
+    extend(Controller.prototype, {
         destroy: function () {
             this._eventManager.destroy();
         },
